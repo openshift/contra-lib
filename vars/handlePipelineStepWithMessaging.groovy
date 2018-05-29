4a85d470e263ba44b60f7b890c1026bd1e784019
jstuart <jstuart@redhat.com>
2018-05-29 12:58:16 -0500

    adding messaging

--- vars/handlePipelineStepWithMessaging.groovy
+++ vars/handlePipelineStepWithMessaging.groovy
@@ -2,8 +2,9 @@
  * Wrapper step to print debugging information and record the time spent running the wrapped code
  * Example Usage:
  *
+ * msgProperties = ['msgTopic': 'stageName', 'msgProps': 'stage=stage1', 'msgContent': '{"pipelineResult":"success"}']
  * stage('mystage') {
- *     handlePipelineStep {
+ *     handlePipelineStepWithMessaging(msgProperties) {
  *         runCode()
  *     }
  * }
@@ -13,20 +14,18 @@
  * @return
  */
 
-import org.contralib.pipeline.Utils
-
 
 def call(Map parameters, Closure body) {
     def measurementName = parameters.get('measurement', env.JOB_NAME)
     def name = parameters.get('stageName', env.STAGE_NAME ?: env.JOB_NAME)
-    def utils = new Utils()
+    def msgProperties = parameters.get('msg', ['msgTopic': null, 'msgProps': null, 'msgContent': null])
 
     try {
         print "running pipeline step: ${name}"
         this.ciMetrics.timed measurementName, name, {
-            utils.sendMessageWithAudit()
+            sendMessageWithAudit(msgProperties)
             body()
-            utils.sendMessageWithAudit()
+            sendMessageWithAudit(msgProperties)
         }
     } catch(e) {
         throw e
