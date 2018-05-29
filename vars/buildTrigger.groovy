206116ed3149a2e19fc8cf75364d7e4a53db0c3b
jstuart <jstuart@redhat.com>
2018-05-29 14:15:47 -0500

    adding docs

--- vars/buildTrigger.groovy
+++ vars/buildTrigger.groovy
@@ -1,3 +1,14 @@
+/**
+ * Wrapper for the validation stage of a build trigger
+ * Example Usage:
+ *
+ * def valid = buildTrigger {
+ *     return runValidation(env.CI_MESSAGE)
+ * }
+ * 
+ * @param body
+ * @return
+ */
 def call(Closure body = {}) {
     def result = false
     handlePipelineStep {
