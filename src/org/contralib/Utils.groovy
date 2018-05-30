86800acfa5719fcf2735d275875e6f35453949ff
jstuart <jstuart@redhat.com>
2018-05-30 16:13:01 -0500

    more utils functions

--- src/org/contralib/Utils.groovy
+++ src/org/contralib/Utils.groovy
@@ -1,3 +1,8 @@
+/**
+ * @param request - the url that refers to the package
+ * @param prefix - env prefix
+ * @return
+ */
 def repoFromRequest(String request) {
 
     def repo = null
@@ -48,4 +53,43 @@ def setBuildBranch(String tag) {
     }
 
     return [branch, repo_branch]
+}
+
+/**
+ * Initialize message audit file
+ * @param auditFile audit file for messages
+ * @return
+ */
+def initializeAuditFile(String auditFile) {
+    // Ensure auditFile is available
+    sh "rm -f ${auditFile}"
+    String msgAuditFileDir = sh(script: "dirname ${auditFile}", returnStdout: true).trim()
+    sh "mkdir -p ${msgAuditFileDir}"
+    sh "touch ${auditFile}"
+    sh "echo '{}' >> ${auditFile}"
+}
+
+/**
+ * Test if $tag tests exist for $mypackage on $mybranch in fedora dist-git
+ * For mybranch, use fXX or master, or PR number (digits only)
+ * @param mypackage
+ * @param mybranch - Fedora branch or PR number
+ * @param tag
+ * @return
+ */
+def checkTests(String mypackage, String mybranch, String tag) {
+    echo "Currently checking if package tests exist"
+    sh "rm -rf ${mypackage}"
+    if (mybranch.isNumber()) {
+        sh "git clone https://src.fedoraproject.org/rpms/${mypackage}"
+        dir("${mypackage}") {
+            sh "git fetch -fu origin refs/pull/${mybranch}/head:pr"
+            sh "git checkout pr"
+            return sh (returnStatus: true, script: """
+            grep -r '\\- '${tag}'\$' tests""") == 0
+        }
+    } else {
+        return sh (returnStatus: true, script: """
+        git clone -b ${mybranch} --single-branch https://src.fedoraproject.org/rpms/${mypackage}/ && grep -r '\\- '${tag}'\$' ${mypackage}/tests""") == 0
+    }
 }
