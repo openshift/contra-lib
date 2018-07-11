3074fa72702f50b40cb1efeb238a4ceb8a27c659
jstuart <jstuart@redhat.com>
2018-07-11 14:18:28 -0500

    updating kojiMessage

--- vars/kojiMessage.groovy
+++ vars/kojiMessage.groovy
@@ -7,7 +7,7 @@ def call(Map parameters = [:]) {
 
     def utils = new Utils()
 
-    def parsedMsg = utils.flattenJSON(message.replace("\n", "\\n"))
+    def parsedMsg = readJSON text: message.replace("\n", "\\n")
 
     parsedMsg['repo'] = utils.repoFromRequest(ci_data['request'][0])
     def branch = utils.setBuildBranch(ci_data['request'][1])
