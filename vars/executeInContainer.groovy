3dfbc92e7c0d8af72e1e02c4a95d0e3466d0404a
jstuart <jstuart@redhat.com>
2018-07-18 12:39:42 -0500

    tostring

--- vars/executeInContainer.groovy
+++ vars/executeInContainer.groovy
@@ -19,7 +19,7 @@ def call(Map parameters) {
     loadProps.each { stage ->
         def jobProps = readProperties file: "${stage}/job.props"
         echo "print job props"
-        echo jobProps
+        echo jobProps.toString()
         echo "listing props"
         sh "cat ${stage}/job.props"
         stageVars << jobProps
