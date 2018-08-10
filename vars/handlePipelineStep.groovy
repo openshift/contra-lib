f517cd48119078475d9d86aa7bc923535176a5c2
jstuart <jstuart@redhat.com>
2018-08-10 14:45:48 -0500

    populate stage metrics

--- vars/handlePipelineStep.groovy
+++ vars/handlePipelineStep.groovy
@@ -21,24 +21,37 @@ def call(Map parameters = [:], Closure body) {
     def stageMsg = parameters.get('stageMsg')
 
     def cimetrics = ciMetrics.metricsInstance
+    def stageStatus = null
+    def runtime = 0.0
 
     try {
 
         print "running pipeline step: ${name}"
-        def runtime = cimetrics.timed measurementName, name, {
+        runtime = cimetrics.timed measurementName, name, {
 
             body()
 
-            if (stageMsg) {
-                def runtimeMsg = ['stage': ['runtime': runtime]]
-                sendMessageWithAudit(stageMsg(runtimeMsg))
-            }
+            stageStatus = 'complete'
 
         }
+
     } catch(e) {
+
+        stageStatus = 'error'
         echo "${env.JOB_NAME} failed in stage: ${name} with error: ${e.toString()}"
         throw e
+
     } finally {
+
+        if (stageMsg) {
+            def runtimeMsg = ['stage': ['runtime': runtime,
+                                        'name': env.STAGE_NAME,
+                                        'status': stageStatus
+                                       ]
+            ]
+            sendMessageWithAudit(stageMsg(runtimeMsg))
+        }
+
         print "end of pipeline step: ${name}"
     }
 }
