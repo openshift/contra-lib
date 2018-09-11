1a1f57d4afc833c3db7ca2b7cb60e450fafd09ec
jstuart <jstuart@redhat.com>
2018-09-11 11:47:19 -0500

    removing quotes

--- src/org/centos/jobdsl/multiBranchJob.groovy
+++ src/org/centos/jobdsl/multiBranchJob.groovy
@@ -6,7 +6,7 @@ class multiBranchJob {
 
     static def commentTrigger(String comment) {
         return {
-            'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject' / 'sources' / 'data' / 'jenkins.branch.BranchSource' << {
+            it / sources / 'data' / 'jenkins.branch.BranchSource' << {
                 strategy(class: 'jenkins.branch.DefaultBranchPropertyStrategy') {
                     properties(class: 'java.util.Arrays$ArrayList') {
                         a(class: 'jenkins.branch.BranchProperty-array') {
