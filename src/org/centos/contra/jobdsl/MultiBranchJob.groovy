6019d737b991b98ba1870c2dafdfd5042fc8272d
jstuart <jstuart@redhat.com>
2018-09-13 07:52:48 -0500

    updating job

--- src/org/centos/contra/jobdsl/MultiBranchJob.groovy
+++ src/org/centos/contra/jobdsl/MultiBranchJob.groovy
@@ -4,7 +4,36 @@ package org.centos.contra.jobdsl
 
 class MultiBranchJob {
 
+    def job
+
+    MultiBranchJob(def job, String name) {
+        this.job = job.multiBranchPipelineJob(name)
+    }
+
+    void addGitHub(String repoName, String owner, String credentials = null) {
+
+        job.with {
+            branchSources {
+                github {
+                    if (credentials) {
+                        scanCredentialsId(credentials)
+                    }
+                    repoOwner(owner)
+                    repository(repoName)
+                }
+            }
+        }
+
+    }
+
+    void addComment(String comment) {
+        job.with {
+            configure commentTrigger(comment)
+        }
+    }
+
     static def commentTrigger(String comment) {
+
         return {
             it / sources / 'data' / 'jenkins.branch.BranchSource' << {
                 strategy(class: 'jenkins.branch.DefaultBranchPropertyStrategy') {
