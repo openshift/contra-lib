945bc62cbd4761e2761de4d9019c60f73c836406
jstuart <jstuart@redhat.com>
2018-10-02 12:05:16 -0500

    debugging

--- vars/releaseTests.groovy
+++ vars/releaseTests.groovy
@@ -2,7 +2,7 @@ import org.centos.contra.pipeline.GitUtils
 
 
 def call(String tag, String msg, String username, String password, String repo) {
-    def gitutils = new GitUtils(username, password, repo)
+    def gitutils = new GitUtils(username, password)
 /*
     gitutils.createRelease(tag, msg)
 
