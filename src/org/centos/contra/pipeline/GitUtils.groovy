189a7fe6a970ddb8dd100074199926bf72a00cd7
jstuart <jstuart@redhat.com>
2018-10-02 11:01:14 -0500

    testing release api

--- src/org/centos/contra/pipeline/GitUtils.groovy
+++ src/org/centos/contra/pipeline/GitUtils.groovy
@@ -4,36 +4,59 @@ package org.centos.contra.pipeline
 import org.kohsuke.github.GitHub
 import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GHPullRequest
+import org.kohsuke.github.GHRelease
 
 
 class GitUtils {
 
     String username
     String password
+    String repoName
     GitHub gitHub
+    GHRepository ghRepository
 
-    GitUtils(String username, String password) {
+    GitUtils(String username, String password, String repo) {
         this.username = username
         this.password = password
-        this.gitHub = null
+        this.repoName = repo
+        this.gitHub = connect()
+        this.ghRepository = this.gitHub.getRepository(this.repoName)
     }
 
     def connect() {
+        def connection = null
         try {
-            gitHub = GitHub.connectUsingPassword(username, password)
+            connection = GitHub.connectUsingPassword(username, password)
         } catch(e) {
             throw new Exception("unable to connect to github: ${e.toString()}")
         }
 
+        return connection
     }
 
-    def mergePR(def repo, def prNumber, def mergeMsg) {
-        if (!gitHub) {
-            connect()
-        }
-
-        GHRepository ghRepository = gitHub.getRepository(repo)
+    def mergePR(def prNumber, def mergeMsg) {
         GHPullRequest ghPullRequest = ghRepository.getPullRequest(prNumber)
         ghPullRequest.merge(mergeMsg)
+    }
+
+    def createRelease(String tag, String releaseMsg) {
+        GHRelease ghRelease = ghRepository.createRelease(tag)
+                                .body(releaseMsg)
+                                .create()
+
+        return ghRelease
+    }
+
+    def getReleaseByTagName(String tag) {
+        GHRelease ghRelease = ghRepository.getReleaseByTagName(tag)
+
+        return ghRelease
+    }
+
+    def getLatestRelease() {
+        GHRelease ghRelease = ghRepository.getLatestRelease()
+
+        return ghRelease
+
     }
 }
