c4b9222c6ebc5296af1376052eda14b77e21468a
jstuart <jstuart@redhat.com>
2018-10-03 16:11:07 -0500

    removing podTemplate

--- vars/buildTestContainer.groovy
+++ vars/buildTestContainer.groovy
@@ -20,7 +20,6 @@ def call(parameters = [:]) {
     def modify_args = parameters.modify_args ?: [:]
     def docker_registry = parameters.docker_registry ?: 'docker://docker.io'
     def docker_namespace = parameters.docker_namespace
-    def send_metrics = parameters.send_metrics ?: false
     def podTemplateProps = parameters.podTemplateProps ?: [:]
     def credentials = parameters.credentials ?: []
     def build_root = parameters.build_root ?: '.'
@@ -31,89 +30,84 @@ def call(parameters = [:]) {
         buildContainer = buildContainer[0]
     }
 
-    deployOpenShiftTemplate(podTemplateProps) {
-        ciPipeline(sendMetrics: send_metrics, decorateBuild: decoratePRBuild()) {
+    def containerWrapper = { cmd ->
+        executeInContainer(containerName: buildContainer, containerScript: cmd, stageVars: [], credentials: credentials)
+    }
 
+    stage('prepare-build') {
+        handlePipelineStep {
+            deleteDir()
 
-            def containerWrapper = { cmd ->
-                executeInContainer(containerName: buildContainer, containerScript: cmd, stageVars: [], credentials: credentials)
-            }
+            // include option to checkout linchpin version
+            checkout scm
 
-            stage('prepare-build') {
-                handlePipelineStep {
-                    deleteDir()
+        }
+    }
 
-                    // include option to checkout linchpin version
-                    checkout scm
+    stage('Build-Docker-Image') {
+        def buildCmd = null
 
-                }
-            }
+        if (build_args) {
+            def joinedArgs = build_args.collect { key, value -> "${key}=${value}"}.join(" --build-arg ")
+            buildCmd = "buildah bud --build-arg ${joinedArgs} -t ${image_name} ${build_root}"
 
-            stage('Build-Docker-Image') {
-                def buildCmd = null
+        } else {
+            buildCmd = "buildah bud -t ${image_name} ${build_root}"
+        }
 
-                if (build_args) {
-                    def joinedArgs = build_args.collect { key, value -> "${key}=${value}"}.join(" --build-arg ")
-                    buildCmd = "buildah bud --build-arg ${joinedArgs} -t ${image_name} ${build_root}"
+        def cmd = """
+        set -x
+        ${buildCmd}
+        buildah from --name ${container_name} ${image_name}
+        """
+        containerWrapper(cmd)
+    }
 
+    stage('test-docker-container') {
+        def cmd = ""
+
+        if (modify_args) {
+            def owner = modify_args['owner']
+            def copyCmds = modify_args['items'].collect { item ->
+                "buildah copy --chown ${owner} ${container_name} ${item[0]} ${item[1]}"
+            }.join('\n')
+
+            cmd = """
+            set -x
+            ${copyCmds}
+            buildah commit ${container_name} ${image_name}-test
+            buildah from --name ${container_name}-test ${image_name}-test
+            buildah run ${container_name}-test -- ${test_cmd}
+            """
+        } else {
+            cmd = """
+            set -x
+            buildah run ${container_name} -- ${test_cmd}
+            """
+        }
+
+        containerWrapper(cmd)
+    }
+
+    if (versions) {
+        stage('Tag-Push-docker-image') {
+            def cmd = 'set -x'<<'\n'
+            versions.each { version ->
+                cmd << "buildah tag ${image_name} ${image_name}:${version}"
+                cmd << "\n"
+
+                if (credentials) {
+                    cmd << "buildah push --creds \${DOCKER_USERNAME}:\${DOCKER_PASSWORD} localhost/${image_name}:${version} ${docker_registry}/${docker_namespace}/${image_name}:${version}"
+                    cmd << "\n"
                 } else {
-                    buildCmd = "buildah bud -t ${image_name} ${build_root}"
+                    cmd << "buildah push localhost/${image_name}:${version} ${docker_registry}/${docker_namespace}/${image_name}:${version}"
+                    cmd << "\n"
                 }
 
-                def cmd = """
-                set -x
-                ${buildCmd}
-                buildah from --name ${container_name} ${image_name}
-                """
-                containerWrapper(cmd)
             }
 
-            stage('test-docker-container') {
-                def cmd = ""
-
-                if (modify_args) {
-                    def owner = modify_args['owner']
-                    def copyCmds = modify_args['items'].collect { item ->
-                        "buildah copy --chown ${owner} ${container_name} ${item[0]} ${item[1]}"
-                    }.join('\n')
-
-                    cmd = """
-                    set -x
-                    ${copyCmds}
-                    buildah commit ${container_name} ${image_name}-test
-                    buildah from --name ${container_name}-test ${image_name}-test
-                    buildah run ${container_name}-test -- ${test_cmd}
-                    """
-                } else {
-                    cmd = """
-                    set -x
-                    buildah run ${container_name} -- ${test_cmd}
-                    """
-                }
-
-                containerWrapper(cmd)
-            }
-
-            if (versions) {
-                stage('Tag-Push-docker-image') {
-                    def cmd = 'set -x'<<'\n'
-                    versions.each { version ->
-                        cmd << "buildah tag ${image_name} ${image_name}:${version}"
-                        cmd << "\n"
-
-                        if (credentials) {
-                            cmd << "buildah push --creds \${DOCKER_USERNAME}:\${DOCKER_PASSWORD} localhost/${image_name}:${version} ${docker_registry}/${docker_namespace}/${image_name}:${version}"
-                            cmd << "\n"
-                        } else {
-                            cmd << "buildah push localhost/${image_name}:${version} ${docker_registry}/${docker_namespace}/${image_name}:${version}"
-                            cmd << "\n"
-                        }
-
-                    }
-
-                    containerWrapper(cmd)
-                }
-            }
+            containerWrapper(cmd)
         }
     }
 }
+
