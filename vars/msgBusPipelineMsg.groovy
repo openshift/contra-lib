ed1c1c2c73bce33b29c0fe04e1707080936cbe28
Johnny Bieren <jbieren@redhat.com>
2018-12-14 10:15:18 -0500

    Add pipeline.* messages
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

---
+++ vars/msgBusPipelineMsg.groovy
@@ -0,0 +1,29 @@
+import org.centos.contra.pipeline.Utils
+
+/**
+ * Defines the pipeline.[running,complete] messages
+ * This will merge parameters with the defaults and will validate each parameter
+ * @param parameters
+ * @return HashMap
+ */
+def call(Map parameters = [:]) {
+
+    def utils = new Utils()
+
+    def defaults = readJSON text: libraryResource('msgBusPipelineMsg.json')
+
+    return { Map runtimeArgs = [:] ->
+        // Set defaults that can't go in json file
+        parameters['ci'] = parameters['ci'] ?: msgBusCIContent()()
+        parameters['run'] = parameters['run'] ?: msgBusRunContent()()
+        parameters['artifact'] = parameters['artifact'] ?: msgBusArtifactContent()()
+        parameters['pipeline'] = parameters['pipeline'] ?: msgBusPipelineContent()()
+        parameters['generated_at'] = parameters['generated_at'] ?: java.time.Instant.now().toString()
+
+        parameters = utils.mapMergeQuotes([parameters, runtimeArgs])
+        mergedMessage = utils.mergeBusMessage(parameters, defaults)
+
+        // sendCIMessage expects String arguments
+        return utils.getMapStringColon(mergedMessage)
+    }
+}
