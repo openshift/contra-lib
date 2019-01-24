5084d888c50c9b87a7594590acea255079e72c04
Johnny Bieren <jbieren@redhat.com>
2019-01-24 11:09:33 -0500

    Add ability to ignore errors
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- vars/kojiMessage.groovy
+++ vars/kojiMessage.groovy
@@ -4,15 +4,24 @@ import org.centos.contra.pipeline.Utils
 def call(Map parameters = [:]) {
 
     def message = parameters.get('message', '{}')
+    def ignoreErrors = parameters.get('ignoreErrors', false)
 
     def utils = new Utils()
 
     def parsedMsg = utils.flattenJSON(message)
 
-    parsedMsg['repo'] = utils.repoFromRequest(parsedMsg['request'][0])
-    def branch = utils.setBuildBranch(parsedMsg['request'][1])
-    parsedMsg['branch'] = branch[0]
-    parsedMsg['repo_branch'] = branch[1]
+    try {
+        parsedMsg['repo'] = utils.repoFromRequest(parsedMsg['request'][0])
+        def branch = utils.setBuildBranch(parsedMsg['request'][1])
+        parsedMsg['branch'] = branch[0]
+        parsedMsg['repo_branch'] = branch[1]
+    } catch (e) {
+        if (ignoreErrors) {
+            println("Ignoring error message: " + e)
+        } else {
+            throw e
+        }
+    }
 
     return parsedMsg
 }
