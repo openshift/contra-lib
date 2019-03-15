fbf61c518cae40004f2fc863dd171d57551a6565
Johnny Bieren <jbieren@redhat.com>
2019-03-15 11:43:13 -0400

    Updates for spec changes
    Signed-off-by: Johnny Bieren <jbieren@redhat.com>

--- vars/msgBusErrorContent.groovy
+++ vars/msgBusErrorContent.groovy
@@ -1,7 +1,7 @@
 import org.centos.contra.pipeline.Utils
 
 /**
- * Defines the header of a message
+ * Defines the error content of a message
  * This will merge parameters with the defaults and will validate each parameter
  * @param parameters
  * @return HashMap
@@ -10,20 +10,20 @@ def call(Map parameters = [:]) {
 
     def utils = new Utils()
 
-    def defaults = readJSON text: libraryResource('msgBusHeader.json')
+    def defaults = readJSON text: libraryResource('msgBusErrorContent.json')
 
     return { Map runtimeArgs = [:] ->
         // Set defaults that can't go in json file
-        parameters['topic'] = parameters['topic'] ?: "org.fedoraproject.prod.ci.koji-build.test.complete"
+        parameters['reason'] = parameters['reason'] ?: "Unknown execution error"
 
-        parameters = utils.mapMerge([parameters, runtimeArgs])
+        parameters = utils.mapMergeQuotes([parameters, runtimeArgs])
         try {
             mergedMessage = utils.mergeBusMessage(parameters, defaults)
         } catch(e) {
-            throw new Exception("Creating the message header failed: " + e)
+            throw new Exception("Creating message for error array failed: " + e)
         }
 
         // sendCIMessage expects String arguments
-        return utils.getMapString(mergedMessage)
+        return utils.getMapStringColon(mergedMessage)
     }
 }
