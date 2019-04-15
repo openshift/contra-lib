f79c7efba7349502082bf5a1a19692bbca91cc26
Johnny Bieren <jbieren@redhat.com>
2019-04-15 10:13:43 -0400

    Use null instead of empty string

--- vars/sendMessageWithAudit.groovy
+++ vars/sendMessageWithAudit.groovy
@@ -13,7 +13,7 @@ def call(Map parameters = [:]) {
     def msgTopic = parameters.get('msgTopic')
     def msgProps = parameters.get('msgProps', '')
     def msgContent = parameters.get('msgContent', '')
-    def msgProvider = parameters.get('provider', '')
+    def msgProvider = parameters.get('provider')
     def msgAuditFile = parameters.get('msgAuditFile', 'auditfile.json')
     def msgRetryCount = parameters.get('msgRetryCount', 3)
 
@@ -31,7 +31,7 @@ def call(Map parameters = [:]) {
 
     // Send message and get handle on SendResult
     def sendResult = null
-    if (msgProvider != '') {
+    if (msgProvider) {
         sendResult = utils.sendMessage(msgTopic, msgProps, msgContent, msgProvider)
     } else {
         sendResult = utils.sendMessage(msgTopic, msgProps, msgContent)
