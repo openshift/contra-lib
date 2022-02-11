87f2f6e624b72e03e0eda8aeee19977cb501fcc8
Pratiksha Patnaik <prpatnai@redhat.com>
2022-02-11 18:05:53 -0500

    fix(CPAAS-3221):Fixing Invalid UMB Content

--- vars/sendMetricsMsg.groovy
+++ vars/sendMetricsMsg.groovy
@@ -26,7 +26,10 @@ def call(Map metricsMap) {
         def params = []
         if (metricsMap['service']['params']) {
             metricsMap['service']['params'].each {
-                params.add("\"$it\"")
+                println(it.getClass())
+                b = "$it".replaceAll('"','')
+                b = "$b".replaceAll("\n","")
+                params.add("\"$b\"")
             }
             metricsMap['service']['params'] = params
         }
